1a266cfa1fbd98ef40f34f0914bb46a61cdb5f36
Virhan Ali <virhannn@gmail.com>
2021-12-10 18:18:23 +0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
+const router = express.Router();
+
+let users = [];
+
+router.get('/', (req, res) => {
+  res.send(users);
+});
+
+router.post('/', (req, res) => {
+  const user = req.body;
+  users.push({ ...user, id: uuidv4() });
+  res.send(`User with the name ${user.firstname} added to the database`);
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
+  res.send("User with the id ${id} deleted from the database.");
+});
+
+export default router;
